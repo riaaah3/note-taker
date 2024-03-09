425c4e42f6a761415daa04c187ce71b74e280e2d
Zariah <zaripull18@gmail.com>
2024-03-09 11:38:10 -0600

    working in routes

---
+++ routes/htmlroutes.js
@@ -0,0 +1,14 @@
+const app = require("express").Router()
+const path = require("path")
+
+//GEt
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+})
+
+app.get("/notes",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../public/notes.html"))
+})
+
+module.exports = app
