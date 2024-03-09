4f3a5a2f828a05cee442b57d48761772dac35173
Zariah <zaripull18@gmail.com>
2024-03-09 11:16:22 -0600

    working on server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 3003
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.static('public'))
+const apiroutes = require('./routes/apiroutes')
+const htmlroutes = require('./routes/htmlroutes')
+app.use(apiroutes)
+app.use(htmlroutes)
+app.listen(PORT, function(){
+    console.log('routes are working')
+})
